702d864a3f6e38a2047c5afdd164e388371e6dd6
MarcoAmantea <marcoamantea18@gmail.com>
2024-12-16 17:05:21 +0100

    feat: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import languages from "../data/languages";
+import { useState } from "react";
 
 function App() {
-
+ const firstLanguage = languages[0]
+ const [selectedLanguage, setSelectedLanguage] = useState(firstLanguage);
   return (
     <>
       <h1 className="mb-3 ms-3">Learn Web Development</h1>
       <div className="container">
         {languages.map((curLanguage) => (
-          <button className="btn btn-primary me-3 mb-3">{curLanguage.title}</button>
+          <button className="btn btn-primary me-3 mb-3" onClick={()=> setSelectedLanguage(curLanguage)}>{curLanguage.title}</button>
         ))}
         <div class="card w-75 mb-3">
           <div class="card-body">
-            
-          <h5 class="card-title">HTML</h5>
-          <p class="card-text">HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
-            
+            <h5 class="card-title">{selectedLanguage.title}</h5>
+            <p class="card-text">{selectedLanguage.description}</p>
           </div>
         </div>
       </div>
